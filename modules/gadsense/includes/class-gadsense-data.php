566c3e84889a6f2a62b862f4a84532aa7e03f497
JeroenSormani <jeroen.sormani@gmail.com>
2016-04-20 08:29:47 +0200

    Code consistency changes for modules/gadsense/includes/class-gadsense-data.php

--- modules/gadsense/includes/class-gadsense-data.php
+++ modules/gadsense/includes/class-gadsense-data.php
@@ -9,35 +9,34 @@ class Advanced_Ads_AdSense_Data {
 	private $resizing;
 
 	private function __construct() {
-
-        $options = get_option(GADSENSE_OPT_NAME, array());
+		$options = get_option( GADSENSE_OPT_NAME, array() );
 
 		// AdSense publisher id
-		if ( ! isset($options['adsense-id']) ) {
-            // check if there is still an old setting
-            // 'gadsense_options' was renamed
-            $old_options = get_option( 'gadsense_options', array() );
-            if ( isset($old_options['adsense_id']) ) {
-                $options['adsense-id'] = $old_options['adsense_id'];
-                $options['limit-per-page'] = $old_options['limit_ads_per_page'];
+		if ( ! isset( $options['adsense-id'] ) ) {
+			// check if there is still an old setting
+			// 'gadsense_options' was renamed
+			$old_options = get_option( 'gadsense_options', array() );
+			if ( isset( $old_options['adsense_id'] ) ) {
+				$options['adsense-id']     = $old_options['adsense_id'];
+				$options['limit-per-page'] = $old_options['limit_ads_per_page'];
 
-                // remove old options
-                delete_option('gadsense_options');
-            } else {
-                $options['adsense-id'] = '';
-                $options['limit-per-page'] = true;
-            }
+				// remove old options
+				delete_option( 'gadsense_options' );
+			} else {
+				$options['adsense-id']     = '';
+				$options['limit-per-page'] = true;
+			}
 
-            update_option(GADSENSE_OPT_NAME, $options);
+			update_option( GADSENSE_OPT_NAME, $options );
 		}
 
-            if ( !isset($options['limit-per-page']) ) {
-                $options['limit-per-page'] = '';
-            }
+			if ( ! isset( $options['limit-per-page'] ) ) {
+				$options['limit-per-page'] = '';
+			}
 
-            if ( !isset($options['page-level-enabled']) ) {
-                $options['page-level-enabled'] = false;
-            }
+			if ( ! isset( $options['page-level-enabled'] ) ) {
+				$options['page-level-enabled'] = false;
+			}
 
 		$this->options = $options;
 
@@ -47,14 +46,15 @@ class Advanced_Ads_AdSense_Data {
 		);
 	}
 
-        /**
-	 * GETTERS
-	 */
+		/**
+		 * GETTERS
+		 */
 	public function get_options() {
 		return $this->options;
 	}
+
 	public function get_adsense_id() {
-		return trim($this->options['adsense-id']);
+		return trim( $this->options['adsense-id'] );
 	}
 
 	public function get_limit_per_page() {
@@ -62,7 +62,7 @@ class Advanced_Ads_AdSense_Data {
 	}
 
 	public function get_responsive_sizing() {
-		$resizing = $this->resizing;
+		$resizing       = $this->resizing;
 		$this->resizing = apply_filters( 'advanced-ads-gadsense-responsive-sizing', $resizing );
 		return $this->resizing;
 	}
