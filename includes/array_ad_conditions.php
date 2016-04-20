d3bc1712f49e9b35d3bc502f6dfc0bb165d26177
JeroenSormani <jeroen.sormani@gmail.com>
2016-04-20 08:29:23 +0200

    Code consistency changes for includes/array_ad_conditions.php

--- includes/array_ad_conditions.php
+++ includes/array_ad_conditions.php
@@ -3,7 +3,7 @@
 /**
  * conditions under which to (not) show an ad
  * I don’t like huge arrays like this to clutter my classes
- *  and anyway, this might be needed on multiple places
+ * and anyway, this might be needed on multiple places
  *
  * at the bottom, you find a filter to be able to extend / remove your own elements
  *
@@ -29,70 +29,70 @@ if ( ! class_exists( 'Advanced_Ads', false ) ) {
 $advanced_ads_slug = Advanced_Ads::get_instance()->get_plugin_slug();
 
 $advanced_ads_ad_conditions = array(
-	'enabled' => array( // dummy, to let the validation of the general ad conditions past
+	'enabled'            => array( // dummy, to let the validation of the general ad conditions past
 		// 'label' => __('Post Types', $advanced_ads_slug),
 		// 'description' => __('Choose the public post types on which to display the ad.', $advanced_ads_slug),
 		'type' => 'other',
 		// 'callback' => array('Advanced_Ads_Display_Condition_Callbacks', 'post_types')
 	),
-	'posttypes' => array(
-		'label' => __( 'Post Types', $advanced_ads_slug ),
+	'posttypes'          => array(
+		'label'       => __( 'Post Types', $advanced_ads_slug ),
 		'description' => __( 'Choose the public post types on which to display the ad.', $advanced_ads_slug ),
-		'type' => 'textvalues',
-		'callback' => array('Advanced_Ads_Display_Condition_Callbacks', 'post_types')
+		'type'        => 'textvalues',
+		'callback'    => array( 'Advanced_Ads_Display_Condition_Callbacks', 'post_types' )
 	),
-	'categoryids' => array(
-		'label' => __( 'Categories, Tags and Taxonomies', $advanced_ads_slug ),
+	'categoryids'        => array(
+		'label'       => __( 'Categories, Tags and Taxonomies', $advanced_ads_slug ),
 		'description' => __( 'Choose terms from public category, tag and other taxonomies a post must belong to in order to have ads.', $advanced_ads_slug ),
-		'type' => 'idfield',
-		'callback' => array('Advanced_Ads_Display_Condition_Callbacks', 'terms')
+		'type'        => 'idfield',
+		'callback'    => array( 'Advanced_Ads_Display_Condition_Callbacks', 'terms' )
 	),
 	'categoryarchiveids' => array(
-		'label' => __( 'Category Archives', $advanced_ads_slug ),
+		'label'       => __( 'Category Archives', $advanced_ads_slug ),
 		'description' => __( 'comma seperated IDs of category archives', $advanced_ads_slug ),
-		'type' => 'idfield',
-		'callback' => array('Advanced_Ads_Display_Condition_Callbacks', 'category_archives')
+		'type'        => 'idfield',
+		'callback'    => array( 'Advanced_Ads_Display_Condition_Callbacks', 'category_archives' )
 	),
-	'postids' => array(
-		'label' => __( 'Individual Posts, Pages and Public Post Types', $advanced_ads_slug ),
+	'postids'            => array(
+		'label'       => __( 'Individual Posts, Pages and Public Post Types', $advanced_ads_slug ),
 		'description' => __( 'Choose on which individual posts, pages and public post type pages you want to display or hide ads.', $advanced_ads_slug ),
-		'type' => 'other',
-		'callback' => array('Advanced_Ads_Display_Condition_Callbacks', 'single_posts')
+		'type'        => 'other',
+		'callback'    => array( 'Advanced_Ads_Display_Condition_Callbacks', 'single_posts' )
 	),
-	'is_front_page' => array(
-		'label' => __( 'Home Page', $advanced_ads_slug ),
+	'is_front_page'      => array(
+		'label'       => __( 'Home Page', $advanced_ads_slug ),
 		'description' => __( 'show on Home page', $advanced_ads_slug ),
-		'type' => 'radio',
+		'type'        => 'radio',
 	),
-	'is_singular' => array(
-		'label' => __( 'Singular Pages', $advanced_ads_slug ),
+	'is_singular'        => array(
+		'label'       => __( 'Singular Pages', $advanced_ads_slug ),
 		'description' => __( 'show on singular pages/posts', $advanced_ads_slug ),
-		'type' => 'radio',
+		'type'        => 'radio',
 	),
-	'is_archive' => array(
-		'label' => __( 'Archive Pages', $advanced_ads_slug ),
+	'is_archive'         => array(
+		'label'       => __( 'Archive Pages', $advanced_ads_slug ),
 		'description' => __( 'show on any type of archive page (category, tag, author and date)', $advanced_ads_slug ),
-		'type' => 'radio',
+		'type'        => 'radio',
 	),
-	'is_search' => array(
-		'label' => __( 'Search Results', $advanced_ads_slug ),
+	'is_search'          => array(
+		'label'       => __( 'Search Results', $advanced_ads_slug ),
 		'description' => __( 'show on search result pages', $advanced_ads_slug ),
-		'type' => 'radio',
+		'type'        => 'radio',
 	),
-	'is_404' => array(
-		'label' => __( '404 Page', $advanced_ads_slug ),
+	'is_404'             => array(
+		'label'       => __( '404 Page', $advanced_ads_slug ),
 		'description' => __( 'show on 404 error page', $advanced_ads_slug ),
-		'type' => 'radio',
+		'type'        => 'radio',
 	),
-	'is_attachment' => array(
-		'label' => __( 'Attachment Pages', $advanced_ads_slug ),
+	'is_attachment'      => array(
+		'label'       => __( 'Attachment Pages', $advanced_ads_slug ),
 		'description' => __( 'show on attachment pages', $advanced_ads_slug ),
-		'type' => 'radio',
+		'type'        => 'radio',
 	),
-	'is_main_query' => array(
-		'label' => __( 'Secondary Queries', $advanced_ads_slug ),
+	'is_main_query'      => array(
+		'label'       => __( 'Secondary Queries', $advanced_ads_slug ),
 		'description' => __( 'allow ads in secondary queries', $advanced_ads_slug ),
-		'type' => 'radio',
+		'type'        => 'radio',
 	)
 );
 
