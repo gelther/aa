1ff3aa75a0193629c6f1d72b8190cbf5b22be578
JeroenSormani <jeroen.sormani@gmail.com>
2016-04-20 08:29:44 +0200

    Code consistency changes for modules/gadsense/config.php

--- modules/gadsense/config.php
+++ modules/gadsense/config.php
@@ -5,11 +5,11 @@
 $path = dirname( __FILE__ );
 
 return array(
-	'classmap' => array(
+	'classmap'   => array(
 		'Advanced_Ads_Ad_Type_Adsense' => $path . '/includes/class-ad-type-adsense.php',
-		'Advanced_Ads_AdSense_Data' => $path . '/includes/class-gadsense-data.php',
-		'Advanced_Ads_AdSense_Admin' => $path . '/admin/admin.php',
-		'Advanced_Ads_AdSense_Public' => $path . '/public/public.php',
+		'Advanced_Ads_AdSense_Data'    => $path . '/includes/class-gadsense-data.php',
+		'Advanced_Ads_AdSense_Admin'   => $path . '/admin/admin.php',
+		'Advanced_Ads_AdSense_Public'  => $path . '/public/public.php',
 	),
 	'textdomain' => null,
 );
