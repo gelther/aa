2c024e4112fc0d0642a0cb42cfc8e956c3b0d813
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-14 21:21:26 +0200

    Code consistency changes for admin/views/ad-submitbox-meta.php

--- admin/views/ad-submitbox-meta.php
+++ admin/views/ad-submitbox-meta.php
@@ -11,16 +11,16 @@ $TZ = self::timezone_get_name( self::get_wp_timezone() );
 	    <?php
 			$month = '<label><span class="screen-reader-text">' . __( 'Month' ) . '</span><select id="advads-exp-mm" name="advanced_ad[expiry_date][month]"' . ">\n";
 			for ( $i = 1; $i < 13; $i = $i + 1 ) {
-				$monthnum = zeroise( $i, 2 );
-				$month .= "\t\t\t" . '<option value="' . $monthnum . '" ' . selected( $curr_month, $monthnum, false ) . '>';
-				$month .= sprintf( _x( '%1$s-%2$s', '1: month number (01, 02, etc.), 2: month abbreviation', 'advanced-ads' ),
+				$monthnum  = zeroise( $i, 2 );
+				$month    .= "\t\t\t" . '<option value="' . $monthnum . '" ' . selected( $curr_month, $monthnum, false ) . '>';
+				$month    .= sprintf( _x( '%1$s-%2$s', '1: month number (01, 02, etc.), 2: month abbreviation', 'advanced-ads' ),
 				$monthnum, $wp_locale->get_month_abbrev( $wp_locale->get_month( $i ) ) ) . "</option>\n";
 			}
 			$month .= '</select></label>';
 
-			$day = '<label><span class="screen-reader-text">' . __( 'Day' ) . '</span><input type="text" id="advads-exp-jj" name="advanced_ad[expiry_date][day]" value="' . $curr_day . '" size="2" maxlength="2" autocomplete="off" /></label>';
-			$year = '<label><span class="screen-reader-text">' . __( 'Year' ) . '</span><input type="text" id="advads-exp-aa" name="advanced_ad[expiry_date][year]" value="' . $curr_year . '" size="4" maxlength="4" autocomplete="off" /></label>';
-			$hour = '<label><span class="screen-reader-text">' . __( 'Hour' ) . '</span><input type="text" id="advads-exp-hh" name="advanced_ad[expiry_date][hour]" value="' . $curr_hour . '" size="2" maxlength="2" autocomplete="off" /></label>';
+			$day    = '<label><span class="screen-reader-text">' . __( 'Day' ) . '</span><input type="text" id="advads-exp-jj" name="advanced_ad[expiry_date][day]" value="' . $curr_day . '" size="2" maxlength="2" autocomplete="off" /></label>';
+			$year   = '<label><span class="screen-reader-text">' . __( 'Year' ) . '</span><input type="text" id="advads-exp-aa" name="advanced_ad[expiry_date][year]" value="' . $curr_year . '" size="4" maxlength="4" autocomplete="off" /></label>';
+			$hour   = '<label><span class="screen-reader-text">' . __( 'Hour' ) . '</span><input type="text" id="advads-exp-hh" name="advanced_ad[expiry_date][hour]" value="' . $curr_hour . '" size="2" maxlength="2" autocomplete="off" /></label>';
 			$minute = '<label><span class="screen-reader-text">' . __( 'Minute' ) . '</span><input type="text" id="advads-exp-mn" name="advanced_ad[expiry_date][minute]" value="' . $curr_minute . '" size="2" maxlength="2" autocomplete="off" /></label>';
 
 		?>
